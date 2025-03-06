aa2e81fda9256a903ea35901ae8369d1fa427cef
Himanshu <himanshughorai000@gmail.com>
2025-03-06 22:58:57 +0530

    if and else is code me be doen so now is next is loop

--- if-else.js
+++ if-else.js
@@ -54,7 +54,7 @@ if (age >= 18) {
 
 var num = -1;
 
-// ====> this is one methode of solution this 
+// ====> this is one methode of solution this
 
 // if (num >= 0) {
 //   if (num > 0) {
@@ -66,17 +66,78 @@ var num = -1;
 //   console.log(`htis number is negitive `);
 // }
 
+// this is the othor wey
 
-// this is the othor wey 
+// if (num === 0){
+//   console.log(`thsi number is zero `);
 
-if (num === 0){
-  console.log(`thsi number is zero `);
-  
-}else if (num > 0 ){
+// }else if (num > 0 ){
 
-  console.log(`thsi is positive number `);
-  
-}else {
-  console.log(`this number is nagitive`);
-  
+//   console.log(`thsi is positive number `);
+
+// }else {
+//   console.log(`this number is nagitive`);
+
+// }
+
+// ==================================
+
+// switch statement
+
+// ==> the switch statement is used ot perform task diffrent  action besed  on different condition
+
+// syntax
+//  switch (expression){
+//   //case value1
+//   break;
+//  }
+
+// example
+
+// var day = "friday";
+
+// switch (day) {
+//   case "monday":
+//     console.log(`thsi is monday`);
+//     break;
+//   case "friday":
+//     console.log(`omg let's have some party today`);
+//     break;
+//   case "sunday":
+//     console.log(` to day we are going to bache `);
+//     default :
+//     console.log(`no condition match `);
+
+// }
+
+/*
+Q: Write a JavaScript switch statement that takes a variable areaOfShapes representing
+different shapes, and based on its value, calculates and logs the area of the corresponding
+shape. Consider three shapes: 'Rectangle,' 'Circle,' and 'Square.' For 'Rectangle:
+use variables a use avariable r as the radius; and for 'Square, ' the provided shape is not recognized, 
+log a available. Test your switch statement with and b set to 5 and 10, respectively. Ensure
+is logged to the console. use variable a as the side length. If message saying, 'Sorry the shape
+ is not areaOfShapes set to 'Square' and sides a that the correct area (25 in this case)
+*/
+
+var areaOfshapes = `rectangle`;
+
+var a = 5;
+var b = 10;
+var result;
+switch (areaOfshapes) {
+  case `sircle`:
+    var r = 2;
+    result = 3.142 * (r * r);
+    console.log(result);
+    break;
+  case `square`:
+    result = a * a;
+    console.log(result);
+    break;
+  case `rectangle`:
+    result = a * b;
+    console.log(result);
 }
+
+console.log(areaOfshapes);
