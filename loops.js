cbcca36a3b6e6287d067e2367eb7df90201d673e
Himanshu <himanshughorai000@gmail.com>
2025-03-07 22:41:10 +0530

    loop code while do while for

---
+++ loops.js
@@ -0,0 +1,59 @@
+// loops
+
+// while loop
+
+// var num = 1 ;
+
+// while (num <= 10 ) {
+//     console.log(num);
+//     num++;
+
+// }
+
+// do while loop
+
+// var num = 1 ;
+
+// do {
+//     console.log(num);
+//     num++;
+
+// }while (num <= 10 )
+
+// for loop
+
+// for (var i= 1 ; i <= 10 ; i++){
+//     console.log(i);
+
+// }
+
+// ? let's create a table of 5
+
+//         var i = 1
+
+//         for (let i = 0; i <= 10; i++) {
+//         // console.log("5 *" + i + " = " + (5 * i));
+//         console.log(`5 * ${i} = ${5*i}`);
+
+//  }
+
+//  var num1 = 5;
+
+//  var num2;
+//  for (num2 = 1 ; num2<= 10; num2++){
+//     if (num1<= 10){
+//         console.log(num1 * num2)
+//     }
+//  }
+
+
+// var userInput;
+// var positiveNumber ;
+
+// do {
+//    userInput = prompt(`place Enter Positiove Number`);
+//     positiveNumber = parseFloat(userInput)
+
+    
+// } while (isNaN(positiveNumber) || positiveNumber < 0 );
+// console.log(positiveNumber);
