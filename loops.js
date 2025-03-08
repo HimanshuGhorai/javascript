6cd1531521944d779bf19c0a7c96648062d3ff40
Himanshu <himanshughorai000@gmail.com>
2025-03-08 08:04:57 +0530

    program To check if a year is a leap year

--- loops.js
+++ loops.js
@@ -46,7 +46,6 @@
 //     }
 //  }
 
-
 // var userInput;
 // var positiveNumber ;
 
@@ -54,6 +53,37 @@
 //    userInput = prompt(`place Enter Positiove Number`);
 //     positiveNumber = parseFloat(userInput)
 
-    
 // } while (isNaN(positiveNumber) || positiveNumber < 0 );
 // console.log(positiveNumber);
+
+// calculate this sum of number for 1 to 10 using for loop ?
+
+// var num = 0
+
+// for(var i = 1 ; i <= 10; i++){
+//     num = num + i;
+// console.log(num);
+
+// }
+// console.log(num);
+
+// console.log("10"-"2");
+
+
+
+
+//!1: program To check if a year is a leap year
+// ? If a year is divisible by 4 and not divisible by 100, or
+// If a year is divisible by 400,
+// then it is a leap year. Otherwise, it is not a leap year.
+
+
+// var year = 2024;
+
+// if ((year % 4 === 0 && year % 100 !== 0) || year % 400 ===0){
+//     console.log('it is a leap year');
+    
+// }else{
+//     console.log(`it is not a leap year`);
+    
+// }
