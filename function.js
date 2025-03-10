1c5c0dd359e9d846b1ad658fadc211cf52cde9c5
Himanshu <himanshughorai000@gmail.com>
2025-03-10 09:18:49 +0530

    add funtion file

---
+++ function.js
@@ -0,0 +1,10 @@
+// function
+function sum(a, b) {
+  return a * b;
+}
+console.log(sum(3, 5));
+
+function greet(name) {
+  console.log("hi " + name + " sir wallcame back");
+} 
+greet("himanshu");
--- loops.js
+++ loops.js
@@ -69,31 +69,22 @@
 
 // console.log("10"-"2");
 
-
-
-
 //!1: program To check if a year is a leap year
 // ? If a year is divisible by 4 and not divisible by 100, or
 // If a year is divisible by 400,
 // then it is a leap year. Otherwise, it is not a leap year.
 
-
 // var year = 2024;
 
 // if ((year % 4 === 0 && year % 100 !== 0) || year % 400 ===0){
 //     console.log('it is a leap year');
-    
+
 // }else{
 //     console.log(`it is not a leap year`);
-    
+
 // }
 
-
-
-
-
-
-// **==> 50 qustions about loops 
+// **==> 50 qustions about loops
 
 /* 
 loops 
@@ -105,13 +96,13 @@ loops
 ----> for of loop 
 */
 
-// 50 qustino 
+// 50 qustino
 
 // 1> print 1 -10 number using for loop ?
 
 // for (var i = 0 ; i <= 10 ; i++){
 //     console.log(i);
-    
+
 // }
 // The loop starts at 1 (i = 1).
 // It runs while i <= 10.
@@ -130,8 +121,51 @@ loops
 
 // 3️> Print numbers from 1 to 10 using a do-while loop.
 
-// do{
 //     var i = 0
+// do{
 // i++
 // console.log(i);
 // }while(i <= 10 )
+
+//4️> Print even numbers from 1 to 20 using a for loop.
+
+// for (let i = 0; i <= 20; i +=2, i++) {
+//     console.log(i);
+// }
+
+//5> Print odd numbers from 1 to 20 using a while loop.
+
+// let i = 0;
+// while (i <= 20) {
+//     console.log(i);
+//     i += 4;
+// }
+
+// 6>  Find the sum of numbers from 1 to 100 using a for loop.
+// var sum = 0;
+// for (let i = 0; i <= 100; i++) {
+//   console.log((sum += i));
+// }
+// 5050
+
+// 7> Find the factorial of a number (e.g., 5!) using a loop.
+var num = 5;
+var factorial = 1;
+
+for (var i = 1; i <= num; i++) {
+    factorial *= i;
+}
+console.log(factorial);
+
+//8️>Reverse a number using a while loop.
+
+let number = 12345;
+let Reverse =0 ;
+
+
+while(number>0){
+    let digit = number % 10 ;
+    Reverse = Reverse * 10 + digit;
+        number = Math.floor(number/10);
+}
+console.log(Reverse);
