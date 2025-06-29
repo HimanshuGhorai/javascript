83a2b83a60b27279fd51c4b265746a22b001aebd
Himanshu Ghorai <himanshughorai000@gmail.com>
2025-06-30 00:22:33 +0530

    calculator project is done to day

---
+++ one/js/variable.js
@@ -0,0 +1,18 @@
+// let welcomeMessage = "Hello Friends";
+
+// console.log(welcomeMessage);
+
+// let bagPrice = (15599 - (10 / 100) * 15599) - 272;
+// let janasPrice = (2499 - (41 / 100) * 2499) - 29;
+
+// let bagValue = Math.round((janasPrice + bagPrice + 25 + 20) * 118 / 100);
+
+// let bagSammary = `your final bag amount is ${bagValue}`;
+
+// console.log(bagSammary);
+
+// document.getElementsByClassName("Click-Me");
+
+// let name = document.querySelectorAll('#push-me');
+// name.style.color = 'red';
+// console.log(name);
---
+++ projects/calculator/js/script.js
@@ -0,0 +1,6 @@
+
+let currentDisply ='';
+
+document.querySelector('#input_id').value = currentDisply;
+
+// alert('this is an alert')
